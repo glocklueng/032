d7b01ffb87cad01a9ae4276ecf60d2f75399286e
arko <arko@47c6005b-331e-4be2-8628-a174f3e7207f>
2011-01-28 07:55:11 +0000

    PWM works! Yay! Don't forget: PWMGenEnable(PWM_BASE, PWM_GEN_1);   :P

--- quad_dk/quad_dk.c
+++ quad_dk/quad_dk.c
@@ -10,6 +10,7 @@
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
+#include "driverlib/pwm.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/timer.h"
 #include "driverlib/uart.h"
@@ -38,6 +39,8 @@ Archecture: ARM Cortex M3
 
 int main(void)
 {
+
+    
     // INIT - Initialization Code 
     // -------------------------- 
     InitADC();
@@ -48,7 +51,7 @@ int main(void)
     //
     // Enable the GPIO port that is used for the on-board LED.
     //
-    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
+    //SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
 
     //
     // Do a dummy read to insert a few cycles after enabling the peripheral.
@@ -59,13 +62,37 @@ int main(void)
     // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
     // enable the GPIO pin for digital function.
     //
-    GPIO_PORTF_DIR_R = 0x08;
-    GPIO_PORTF_DEN_R = 0x08;
+    //GPIO_PORTF_DIR_R = 0x08;
+    //GPIO_PORTF_DEN_R = 0x08;
     // --------------------------
                      
     
     unsigned long adc_values[3];
     unsigned long blink_delay = 0;
+     
+    // -----------------------------------------   
+    
+    
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_64);
+    
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM);
+    
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD);
+    
+    GPIOPinConfigure(GPIO_PF3_PWM3);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+    
+    PWMGenConfigure(PWM_BASE, PWM_GEN_1, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+    
+    PWMGenPeriodSet(PWM_BASE, PWM_GEN_1, 7812);
+    PWMPulseWidthSet(PWM_BASE, PWM_OUT_3, 7812 / 4);
+    
+    
+    PWMOutputState(PWM_BASE, PWM_OUT_3_BIT, true);
+    
+    PWMGenEnable(PWM_BASE, PWM_GEN_1);
+    
     
     // MAIN LOOP           
     while(1)
@@ -84,11 +111,18 @@ int main(void)
         ADCIntClear(ADC_BASE, 0);
         
         
-        blink_delay = adc_values[2] * 200;
+        blink_delay = adc_values[2] * 20;
+        
+        if(blink_delay != 0 && blink_delay < 20460)
+        {
+          PWMGenPeriodSet(PWM_BASE, PWM_GEN_1, 20460);
+          PWMPulseWidthSet(PWM_BASE, PWM_OUT_3, blink_delay);
+        }
+        
         //                                          
         // Turn on the LED.
         //
-        GPIO_PORTF_DATA_R |= 0x08;
+        //GPIO_PORTF_DATA_R |= 0x08;
 
         //
         // Delay for a bit.
@@ -102,14 +136,14 @@ int main(void)
         //
         // Turn off the LED.
         //
-        GPIO_PORTF_DATA_R &= ~(0x08);
+        //GPIO_PORTF_DATA_R &= ~(0x08);
 
         //
         // Delay for a bit.
         //
-        for(ulLoop = 0; ulLoop < blink_delay; ulLoop++)
-        {
-        }
+        //for(ulLoop = 0; ulLoop < blink_delay; ulLoop++)
+        //{
+        //}
         
     }
 }
