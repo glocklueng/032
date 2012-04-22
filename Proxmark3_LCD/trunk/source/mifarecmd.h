c598186110d541e7af354252710d5ea5405e8a0f
charliex <charliex@47c6005b-331e-4be2-8628-a174f3e7207f>
2012-04-22 23:27:18 +0000

    updating to newest codebase

--- Proxmark3_LCD/trunk/source/mifarecmd.h
+++ Proxmark3_LCD/trunk/source/mifarecmd.h
@@ -1,16 +1,28 @@
 //-----------------------------------------------------------------------------
+// Merlok - June 2011
+// Gerhard de Koning Gans - May 2008
+// Hagen Fritsch - June 2010
+//
 // This code is licensed to you under the terms of the GNU GPL, version 2 or,
 // at your option, any later version. See the LICENSE.txt file for the text of
 // the license.
 //-----------------------------------------------------------------------------
-// Fonts for the LCD
+// Routines to support ISO 14443 type A.
 //-----------------------------------------------------------------------------
 
-#ifndef __FONTS_H
-#define __FONTS_H
+#ifndef __MIFARECMD_H
+#define __MIFARECMD_H
 
-extern const char FONT6x8[97][8];
-extern const char FONT8x8F[97][8];
-extern const char FONT8x16[97][16];
+#include "proxmark3.h"
+#include "apps.h"
+#include "util.h"
+#include "string.h"
+
+#include "iso14443crc.h"
+#include "iso14443a.h"
+#include "crapto1.h"
+#include "mifareutil.h"
+#include "common.h"
+
 
 #endif
