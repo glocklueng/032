f9f90ab3f360de36aa427efbbdf98f4c47f1ef3a
charliex <charliex@47c6005b-331e-4be2-8628-a174f3e7207f>
2013-02-21 18:14:49 +0000

    tidying up

--- NSL_Tracker/code/gps_module/habex2.c
+++ NSL_Tracker/code/gps_module/habex2.c
@@ -1,10 +1,14 @@
+// need this for delay
 #define F_CPU ( 16000000UL )
 
+// avr specific
 #include <avr/io.h> 
 #include <avr/interrupt.h>
-#include <avr/delay.h>
 
-// macros
+// utilities
+#include <util/delay.h>
+
+// handy macros
 #define SET_BIT(p,m) ((p) |= (m))
 #define CLEAR_BIT(p,m) ((p) &= ~(m))
 
@@ -15,9 +19,15 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
+//////////////////////////////////////////////////////////////////
+//
+// USART code
+//
+//////////////////////////////////////////////////////////////////
+
 // Define baud rate 
-#define USART_BAUDRATE ( 57600 )
-#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
+#define USART_BAUDRATE	( 57600UL )
+#define BAUD_PRESCALE	((( F_CPU / ( USART_BAUDRATE * 16UL ))) - 1 ) 
 
 
 /*! \brief Initialize the USART0 for the communication bus 
@@ -54,13 +64,17 @@ void usart0_transmit(char data ) {
  * \param data The string of characters you wish to send
  * \param length The length of the string you wish to send
  */
-unsigned char usart0_sendstring(char *data,unsigned char length) {
+unsigned char usart0_sendstring(const char *data) {
 	
-	int i;
-	
-	for (i=0;i<length;i++)
+	// send ASCIZ string	
+	while(*data != 0 ) {
+
 		usart0_transmit(*(data++));
 	
+		// delay in transmit
+		_delay_ms( 1 );
+	}	
+
 	return 0;
 }
 
@@ -100,6 +114,7 @@ unsigned char poll_usart0_receive(void ) {
 	/* Check if data is received */
 	return ((UCSR0A & (1<<RXC0)));
 }
+
 /*! \brief Initialize the USART3 for the radio interface
  * This function is used to initialize the USART which a baudrate
  * that needs to be sent in as a parameter Use the baudrate settings
@@ -127,10 +142,13 @@ void usart3_init(unsigned int baudrate) {
  * \param data The character you want to send
  */
 unsigned char usart3_transmit(char  data ) {
+
 	/* Wait for empty transmit buffer */
 	while (!( UCSR3A & (1<<UDRE3)));
+
 	/* Put data into buffer, sends the data */
 	UDR3 = data;
+
 	return 0;
 }
 
@@ -139,13 +157,17 @@ unsigned char usart3_transmit(char  data ) {
  * \param data The string of characters you wish to send
  * \param length The length of the string you wish to send
  */
-unsigned char usart3_sendstring(char *data) {
-	int i;
-	
+unsigned char usart3_sendstring(const char *data) {
+
+	// send ASCIZ string	
 	while(*data != 0 ) {
+
 		usart3_transmit(*(data++));
-		_delay_ms( 10 );
+	
+		// delay in transmit
+		_delay_ms( 1 );
 	}
+
 	return 0;
 }
 
@@ -220,76 +242,89 @@ ISR(USART0_RX_vect)
 
 }
 
+/////////////////////////////////////////////////////
+//
+// Main code
+//
+/////////////////////////////////////////////////////
+
+// status of PC3 line
 static unsigned char pc3on = 0;
-static const char string_at_gps_inf[]="AT+CGPSINF=0\r\n";
+
+// AT CMD's
+static const char string_at_gps_inf[]	= "AT+CGPSINF=0\r\n";
+static const char gsm_init_string[]		= "AT+CNUM\r\nATE1\r\n";
+
+// Boot string
+static const char string_hi[] = "\r\n\r\n[NULL SPACE LABS] 032.la\r\n\r\nSYSTEM BOOTING\r\n\r\n";
 
 int main(void) 
 { 
 	static unsigned int counter = 0;
 	
+	// initialise the two UARTS used
 	usart0_init( BAUD_PRESCALE );
 
 	usart3_init( BAUD_PRESCALE ) ;
 
 	// say hi
-	static const char string_hi[] = "\r\n\r\n[NULL SPACE LABS] 032.la\r\n\r\nSYSTEM BOOTING\r\n\r\n";
-	usart0_sendstring(string_hi,sizeof(string_hi) );
+	usart0_sendstring( string_hi );
 
-	//outputs
-	SET_BIT( DDRD, _BV(PD7) );
+	// setup outputs
+
+	// power on
+	SET_BIT( DDRD,  _BV(PD7) );
 	SET_BIT( PORTD, _BV(PD7) );
 	
+	// setup inputs
 
-	//inputs
-	CLEAR_BIT( DDRC, _BV(PC3) );
-	SET_BIT( PORTC, _BV(PC3) );
+	// first switch
+	CLEAR_BIT( DDRC,  _BV(PC3) );
+	SET_BIT  ( PORTC, _BV(PC3) );
 
+	// state of PC3
 	pc3on = 1;
 	
-	CLEAR_BIT( DDRC, _BV(PC0) );
-  	SET_BIT( PORTC, _BV(PC0) );
+	// second switch
+	CLEAR_BIT( DDRC,  _BV(PC0) );
+  	SET_BIT  ( PORTC, _BV(PC0) );
 
-	_delay_ms(1500);
+	// give it time to warmup
+	_delay_ms( 1500 );
 
-	// init GPS
-	static const char gsm_init_string[] = "AT+CNUM\r\nATE1\r\n";
-	usart3_sendstring( gsm_init_string);
+	// init GPS etc
+	usart3_sendstring( gsm_init_string );
 
 	//Start interrupts
 	sei();
 	
-	for(;;) 
-	{ 
+	// main loop
+	for(;;) { 
 
-		// switch pressed?
+		// switch pressed ?
 		if( bit_is_clear( PINC, PC3 ) ) {
 			
 			if(  pc3on == 0 ) {
 			
 				SET_BIT(PORTD, PD7);
-				usart0_sendstring("PC3 SET\r\n",9);
 				pc3on = 1;
 				
 			} else {
 				
 				CLEAR_BIT(PORTD, PD7);
-				usart0_sendstring("PC3 CLEAR\r\n",11);
 				pc3on = 0;
 			}
-			
+
+			// delay, simple debouncer
+			_delay_ms( 500 );	
+	
 		}
 		
+		// if button is pressed, send this gps string
 		if( bit_is_clear( PINC, PC0 ) ) {
 			usart3_sendstring( string_at_gps_inf );
 		}
-					
-		_delay_ms( 500 );	
-	
+						
 		counter++ ;
-		
-		if( counter == 10 ) {
-			//usart3_sendstring( string_at_gps_inf);
-			counter = 0;
-		}		
 	}
 }			
