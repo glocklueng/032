55b0324c2727553cf7f5d51212d1b0282583f8a0
charliex <charliex@47c6005b-331e-4be2-8628-a174f3e7207f>
2014-05-24 03:32:27 +0000

    transfer

---
+++ LayerOne_2014/proxmark3/armsrc/version.c
@@ -0,0 +1,10 @@
+#include "proxmark3.h"
+/* Generated file, do not edit */
+const struct version_information __attribute__((section(".version_information"))) version_information = {
+	VERSION_INFORMATION_MAGIC,
+	1,
+	1,
+	2,
+	"master/v1.0.0-3-gcba867f-dirty",
+	"2014-05-24 03:03:47",
+};
