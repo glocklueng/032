a3769bd539a192ee96b8897e4062fb0bce638536
charliex <charliex@47c6005b-331e-4be2-8628-a174f3e7207f>
2014-05-25 18:12:14 +0000

    hello

--- LayerOne_2014/proxmark3/armsrc/version.c
+++ LayerOne_2014/proxmark3/armsrc/version.c
@@ -1,10 +1,17 @@
 #include "proxmark3.h"
 /* Generated file, do not edit */
+#ifdef GCC
+
 const struct version_information __attribute__((section(".version_information"))) version_information = {
+#else
+#pragma section=".version_information"
+const struct version_information version_information = {
+#endif
+
 	VERSION_INFORMATION_MAGIC,
 	1,
 	1,
 	2,
 	"master/v1.0.0-3-gcba867f-dirty",
-	"2014-05-25 11:04:37",
+	"2014-05-24 20:18:18",
 };
