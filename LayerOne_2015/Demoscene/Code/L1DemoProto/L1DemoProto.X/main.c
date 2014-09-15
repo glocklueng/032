b64f5a24b46745b3738c63a8c59270af740d39a2
arko <arko@47c6005b-331e-4be2-8628-a174f3e7207f>
2014-09-15 00:05:35 +0000

    Color bars are close, but too many and fuzzy.

--- LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/main.c
+++ LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/main.c
@@ -15,35 +15,44 @@
 _CONFIG1(FWDTEN_OFF)
 _CONFIG2(FNOSC_FRCPLL & PLL96MHZ_ON)
 
+int HOR_RES = 320;
+int VER_RES = 240;
+int HOR_FRONT_PORCH = 20;
+int HOR_BACK_PORCH = 38;
+int HOR_PULSE_WIDTH  = 30;
+int VER_FRONT_PORCH = 4;
+int VER_BACK_PORCH = 15;
+int VER_PULSE_WIDTH = 3;
+
 void config_graphics(void) {
     CLKDIVbits.RCDIV = 0;
     CLKDIVbits.G1CLKSEL = 1;    /* Use 96Mhz Clk */
-    CLKDIV2bits.GCLKDIV = 11;   
+    CLKDIV2bits.GCLKDIV = 4;  // 16 for 30Hz, 6 for 60Hz
 
     CLKDIVbits.PLLEN = 1;
 
     G1CON2bits.DPMODE = 1;      /* TFT */
     G1DBENbits.GDBEN = 0xFFFF;
-    G1DPWbits.DPW = 240;
-    G1DPHbits.DPH = 320;
-    G1DPWTbits.DPWT = 280;
-    G1DPHTbits.DPHT = 326;
-    G1CON3bits.DPCLKPOL = 0;    /* Sample on falling edge*/
+    G1DPWbits.DPW = 320;
+    G1DPHbits.DPH = 240;
+    G1DPWTbits.DPWT = 20 + 30 + 38 + 320;
+    G1DPHTbits.DPHT = 4 + 3 + 15 + 240;
+    G1CON3bits.DPCLKPOL = 1;    /* Sample on falling edge*/
     G1CON3bits.DPENOE = 1;
     G1CON3bits.DPENPOL = 1;
     
     G1CON3bits.DPVSOE = 1;      
     G1CON3bits.DPHSOE = 1;     
-    G1CON3bits.DPVSPOL = 1;     
-    G1CON3bits.DPHSPOL = 1;    
-    G1ACTDAbits.ACTLINE = 6;
-    G1ACTDAbits.ACTPIX = 40;
-    G1VSYNCbits.VSST = 2;
-    G1HSYNCbits.HSST = 10;
-    G1VSYNCbits.VSLEN = 3;
-    G1HSYNCbits.HSLEN = 10;
-    G1DBLCONbits.VENST = 6;
-    G1DBLCONbits.HENST = 40;
+    G1CON3bits.DPVSPOL = 0;
+    G1CON3bits.DPHSPOL = 0;
+    G1ACTDAbits.ACTLINE = 4 + 3 + 15;    // ver sum
+    G1ACTDAbits.ACTPIX = 20 + 30 + 38;    // hor sum
+    G1VSYNCbits.VSST = 4;   // front
+    G1HSYNCbits.HSST = 20;
+    G1VSYNCbits.VSLEN = 3;  //pulse
+    G1HSYNCbits.HSLEN = 30;
+    G1DBLCONbits.VENST = 4 + 3 + 15;
+    G1DBLCONbits.HENST = 20 + 30 + 38;
 
     G1DPADRL = (unsigned long)(0x0000) & 0xFFFF;
     G1DPADRH = 0;
