7c9e9ce7298b81b08149e4615257a949255a2ab3
arko <arko@47c6005b-331e-4be2-8628-a174f3e7207f>
2014-09-17 00:58:55 +0000

    Successful 640x480 at 60Hz

--- LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/main.c
+++ LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/main.c
@@ -13,31 +13,32 @@
 //#include <libpic30.h>
 
 _CONFIG1(FWDTEN_OFF)
-_CONFIG2(FNOSC_FRCPLL & PLL96MHZ_ON)
+_CONFIG2(FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_DIV2)
 
-int HOR_RES = 320;
-int VER_RES = 240;
-int HOR_FRONT_PORCH = 20;
-int HOR_BACK_PORCH = 38;
-int HOR_PULSE_WIDTH  = 30;
-int VER_FRONT_PORCH = 4;
-int VER_BACK_PORCH = 15;
-int VER_PULSE_WIDTH = 3;
+#define HOR_RES 640
+#define VER_RES 480
+#define HOR_FRONT_PORCH 20
+#define HOR_BACK_PORCH 51
+#define HOR_PULSE_WIDTH  96
+#define VER_FRONT_PORCH 12
+#define VER_BACK_PORCH 36
+#define VER_PULSE_WIDTH 2
 
 void config_graphics(void) {
-    CLKDIVbits.RCDIV = 0;
-    CLKDIVbits.G1CLKSEL = 1;    /* Use 96Mhz Clk */
-    CLKDIV2bits.GCLKDIV = 4;  // 16 for 30Hz, 6 for 60Hz
+    
+    /*
+    CLKDIVbits.G1CLKSEL = 1;    // Use 96Mhz Clk
+    CLKDIV2bits.GCLKDIV = 56;  // 16 for 30Hz, 6 for 60Hz
 
     CLKDIVbits.PLLEN = 1;
 
-    G1CON2bits.DPMODE = 1;      /* TFT */
+    G1CON2bits.DPMODE = 1;      // TFT
     G1DBENbits.GDBEN = 0xFFFF;
     G1DPWbits.DPW = 320;
     G1DPHbits.DPH = 240;
-    G1DPWTbits.DPWT = 20 + 30 + 38 + 320;
-    G1DPHTbits.DPHT = 4 + 3 + 15 + 240;
-    G1CON3bits.DPCLKPOL = 1;    /* Sample on falling edge*/
+    G1DPWTbits.DPWT = 99 + 320;
+    G1DPHTbits.DPHT = 22 + 240;
+    G1CON3bits.DPCLKPOL = 1;    // Sample on falling edge
     G1CON3bits.DPENOE = 1;
     G1CON3bits.DPENPOL = 1;
     
@@ -45,19 +46,19 @@ void config_graphics(void) {
     G1CON3bits.DPHSOE = 1;     
     G1CON3bits.DPVSPOL = 0;
     G1CON3bits.DPHSPOL = 0;
-    G1ACTDAbits.ACTLINE = 4 + 3 + 15;    // ver sum
-    G1ACTDAbits.ACTPIX = 20 + 30 + 38;    // hor sum
+    G1ACTDAbits.ACTLINE = 22;    // ver sum
+    G1ACTDAbits.ACTPIX = 88;    // hor sum
     G1VSYNCbits.VSST = 4;   // front
     G1HSYNCbits.HSST = 20;
     G1VSYNCbits.VSLEN = 3;  //pulse
     G1HSYNCbits.HSLEN = 30;
-    G1DBLCONbits.VENST = 4 + 3 + 15;
-    G1DBLCONbits.HENST = 20 + 30 + 38;
+    G1DBLCONbits.VENST = 4;
+    G1DBLCONbits.HENST = 20;
 
     G1DPADRL = (unsigned long)(0x0000) & 0xFFFF;
     G1DPADRH = 0;
 
-    G1CON2bits.DPBPP = 3;     /* 8bpp mode */
+    G1CON2bits.DPBPP = 3;     // 8bpp mode
     G1CON1bits.PUBPP = 3;
 
     G1CON3bits.DPPINOE = 1;
@@ -65,10 +66,49 @@ void config_graphics(void) {
     G1CON3bits.DPPWROE = 1;
 
     G1CON2bits.DPTEST = 2;
+    */
+    
+    CLKDIVbits.RCDIV = 0;
 
-    G1CON1bits.G1EN = 1;        /* Enable module */
+    CLKDIVbits.G1CLKSEL = 1;    /* Use 96Mhz Clk */
+    CLKDIV2bits.GCLKDIV = 11;   /* 56 = divide by 15 = 6.5Mhz */
+
+    G1CON2bits.DPMODE = 1;      /* TFT */
+    G1DBENbits.GDBEN = 0xFFFF;
+    G1DPWbits.DPW = HOR_RES;
+    G1DPHbits.DPH = VER_RES;
+    G1DPWTbits.DPWT = HOR_FRONT_PORCH + HOR_PULSE_WIDTH + HOR_BACK_PORCH + HOR_RES;
+    G1DPHTbits.DPHT = VER_FRONT_PORCH + VER_PULSE_WIDTH + VER_BACK_PORCH + VER_RES;
+    G1CON3bits.DPCLKPOL = 1;    /* Sample on rising edge*/
+    G1CON3bits.DPENOE = 0;
+    G1CON3bits.DPPWROE = 0;
+    G1CON3bits.DPVSOE = 1;      /* use VSYNC */
+    G1CON3bits.DPHSOE = 1;      /* use HSYNC */
+    G1CON3bits.DPVSPOL = 0;     /* VSYNC negative polarity */
+    G1CON3bits.DPHSPOL = 0;     /* HSYNC negative polarity */
+    G1ACTDAbits.ACTLINE = VER_FRONT_PORCH + VER_PULSE_WIDTH + VER_BACK_PORCH;
+    G1ACTDAbits.ACTPIX = HOR_FRONT_PORCH + HOR_PULSE_WIDTH + HOR_BACK_PORCH;
+    G1VSYNCbits.VSST = VER_FRONT_PORCH;
+    G1HSYNCbits.HSST = HOR_FRONT_PORCH;
+    G1VSYNCbits.VSLEN = VER_PULSE_WIDTH;
+    G1HSYNCbits.HSLEN = HOR_PULSE_WIDTH;
+    G1DBLCONbits.VENST = VER_FRONT_PORCH + VER_PULSE_WIDTH + VER_BACK_PORCH;
+    G1DBLCONbits.HENST = HOR_FRONT_PORCH + HOR_PULSE_WIDTH + HOR_BACK_PORCH;
+
+    G1DPADRL = (unsigned long)(0x0000) & 0xFFFF;
+    G1DPADRH = 0;
+
+    G1CON2bits.DPBPP = 2;     /* 8bpp mode */
+    G1CON1bits.PUBPP = 2;
+
+    G1CON3bits.DPPINOE = 1;
     G1CON3bits.DPPOWER = 1;
 
+    G1CON2bits.DPTEST = 2;
+    
+    G1CON1bits.G1EN = 1;        /* Enable module */
+    G1CON3bits.DPPOWER = 1;
+    G1CLUTbits.CLUTEN = 1;
 }
 
 
@@ -87,7 +127,7 @@ int main(void) {
     config_graphics();
 
     while (1) {
-        G1CON2bits.DPTEST = 2;
+        //G1CON2bits.DPTEST = 2;
     }
 
     return 0;
