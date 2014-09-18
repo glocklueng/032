24617014a3cf914fedbda873bc4e98fffeb4d0e5
arko <arko@47c6005b-331e-4be2-8628-a174f3e7207f>
2014-09-18 07:59:12 +0000

    First demo "Bad Rain". Screen size is 190x480 @ 60hz. However we use the first 190 vertical lines so we effectively get 190x190.

--- LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/main.c
+++ LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/main.c
@@ -4,7 +4,7 @@
  *
  * Created on September 8, 2014, 7:25 PM
  */
-
+#include <stdint.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <xc.h>
@@ -15,14 +15,29 @@
 _CONFIG1(FWDTEN_OFF)
 _CONFIG2(FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_DIV2)
 
-#define HOR_RES 320           //320      //640
-#define VER_RES 240           //240      //480
-#define HOR_FRONT_PORCH 10    //10       //20
-#define HOR_BACK_PORCH 22     //22       //51
-#define HOR_PULSE_WIDTH  46   //46       //96
-#define VER_FRONT_PORCH 6     //6        //12
-#define VER_BACK_PORCH 9      //9        //36
-#define VER_PULSE_WIDTH 1     //1        //2
+#define HOR_RES 190           //320      //640
+#define VER_RES 480           //240      //480
+#define HOR_FRONT_PORCH 355   //10       //20
+#define HOR_BACK_PORCH 51     //22       //51
+#define HOR_PULSE_WIDTH  80   //46       //96
+#define VER_FRONT_PORCH 12    //6        //12
+#define VER_BACK_PORCH 21     //9        //36
+#define VER_PULSE_WIDTH 2     //1        //2
+
+#define GFX_DISPLAY_PIXEL_COUNT    91200
+
+#define GFX_DISPLAY_BUFFER_START_ADDRESS        0x00000001ul
+#define GFX_DISPLAY_BUFFER_LENGTH               0x00012C00ul
+
+//__eds__ unsigned char GFXDisplayBuffer[GFX_DISPLAY_PIXEL_COUNT] __attribute__((eds));
+//__eds__ unsigned char GFXDisplayBufferBottom[GFX_DISPLAY_PIXEL_COUNT] __attribute__((eds));
+
+
+__eds__ unsigned char  __attribute__((far,section("eds1b"),space(eds) ,address(0x01000))) GFXDisplayBuffer[ GFX_DISPLAY_PIXEL_COUNT ]; // Sample data buffer
+//__eds__ unsigned char  __attribute__((far,section("eds2b"),space(eds) ,address(0x09000))) GFXDisplayBufferBottom[ GFX_DISPLAY_PIXEL_COUNT ]; // Sample data buffer
+
+
+
 
 void config_graphics(void) {
     
@@ -71,7 +86,7 @@ void config_graphics(void) {
     CLKDIVbits.RCDIV = 0;
 
     CLKDIVbits.G1CLKSEL = 1;                         /* Use 96Mhz Clk */
-    CLKDIV2bits.GCLKDIV = 56;  //56      //11        /* 56 = divide by 15 = 6.5Mhz */
+    CLKDIV2bits.GCLKDIV = 11;  //56      //11        /* 56 = divide by 15 = 6.5Mhz */
 
     G1CON2bits.DPMODE = 1;      /* TFT */
     G1DBENbits.GDBEN = 0xFFFF;
@@ -94,21 +109,33 @@ void config_graphics(void) {
     G1HSYNCbits.HSLEN = HOR_PULSE_WIDTH;
     G1DBLCONbits.VENST = VER_FRONT_PORCH + VER_PULSE_WIDTH + VER_BACK_PORCH;
     G1DBLCONbits.HENST = HOR_FRONT_PORCH + HOR_PULSE_WIDTH + HOR_BACK_PORCH;
-
-    G1DPADRL = (unsigned long)(0x0000) & 0xFFFF;
+    
+    G1DPADRL = (unsigned long)(GFXDisplayBuffer) & 0xFFFF;
     G1DPADRH = 0;
 
-    G1CON2bits.DPBPP = 2;     /* 8bpp mode */
-    G1CON1bits.PUBPP = 2;
+    G1CON2bits.DPBPP = 3;     /* 8bpp mode */
+    G1CON1bits.PUBPP = 3;
 
     G1CON3bits.DPPINOE = 1;
     G1CON3bits.DPPOWER = 1;
 
-    G1CON2bits.DPTEST = 2;
+    //G1CON2bits.DPTEST = 2;
     
     G1CON1bits.G1EN = 1;        /* Enable module */
     G1CON3bits.DPPOWER = 1;
     G1CLUTbits.CLUTEN = 1;
+
+    G1CLUTbits.CLUTEN = 1;
+
+    G1CLUTbits.CLUTRWEN = 1;
+    G1CLUTbits.CLUTADR = 0x00;
+    G1CLUTWR = 0x001F;
+    G1CLUTbits.CLUTADR = 0x01;
+    G1CLUTWR = 0xF800;
+    G1CLUTbits.CLUTADR = 0x02;
+    G1CLUTWR = 0x07E0;
+
+    G1CLUTbits.CLUTRWEN = 0; 
 }
 
 
@@ -124,9 +151,67 @@ int main(void) {
     ANSF = 0x0000;
     ANSG = 0x0000;
     //TRISE = 0x0000;
-    config_graphics();
 
+    uint32_t y;
+    for (y = 1; y < GFX_DISPLAY_PIXEL_COUNT-1; y++)
+    {
+        GFXDisplayBuffer[(unsigned long)(y)] = 0x00;
+    }
+
+    y=0;
+    unsigned char x = 0;
+    for (y = 1; y < GFX_DISPLAY_PIXEL_COUNT-1; y++)
+    {
+      
+            if (y < (uint32_t)6844)
+            {
+                GFXDisplayBuffer[(unsigned long)(y)] = 0x00;
+            }
+            else if(y < (uint32_t)42955)
+            {
+               GFXDisplayBuffer[(unsigned long)(y)] = (unsigned char)x;
+               x=x+60;
+            }
+            else
+            {
+                GFXDisplayBuffer[(unsigned long)(y)] = 0x00;
+            }
+        
+    }
+    config_graphics();
+    
+    
+   
+    x = 0;
     while (1) {
+        y=0;
+
+        for (y = 1; y < GFX_DISPLAY_PIXEL_COUNT-1; y++)
+        {
+
+                if (y < (uint32_t)6844)
+                {
+                    GFXDisplayBuffer[(unsigned long)(y)] = 0x00;
+                }
+                else if(y < (uint32_t)42955)
+                {
+                   if((y-6844) % 332)
+                   {
+                       GFXDisplayBuffer[(unsigned long)(y)] = 0x00;
+                   }
+                   else
+                   {
+                       GFXDisplayBuffer[(unsigned long)(y)] = (unsigned char)x;
+                       x=x+60;
+                   }
+                }
+                else
+                {
+                    GFXDisplayBuffer[(unsigned long)(y)] = 0x00;
+                }
+
+        }
+        x++;
         //G1CON2bits.DPTEST = 2;
     }
 
--- LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/nbproject/Makefile-default.mk
+++ LayerOne_2015/Demoscene/Code/L1DemoProto/L1DemoProto.X/nbproject/Makefile-default.mk
@@ -86,7 +86,7 @@ ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -mlarge-arrays -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 else
@@ -94,7 +94,7 @@ ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d"      -mno-eds-warn  -g -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d"      -mno-eds-warn  -g -omf=elf -mlarge-arrays -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
@@ -116,12 +116,12 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -omf=elf -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,--defsym=__ICD2RAM=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_PK3=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem$(MP_EXTRA_LD_POST) 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -omf=elf -mlarge-arrays -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,--defsym=__ICD2RAM=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_PK3=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem$(MP_EXTRA_LD_POST) 
 	
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem$(MP_EXTRA_LD_POST) 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -mlarge-arrays -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem$(MP_EXTRA_LD_POST) 
 	${MP_CC_DIR}\\xc16-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/L1DemoProto.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf  
 	
 endif
